a4fd94562c24f7f0afcaf4d8a1d0ec75c51d66ec
nurlykhan <nurlykhan.salamatuly@samgau.com>
2024-09-07 12:43:29 +0500

    feat: implement grpc getuserbyid

--- user-service/internal/transport/grpc/server.go
+++ user-service/internal/transport/grpc/server.go
@@ -1,13 +1,22 @@
 package grpc
 
 import (
+	"context"
 	"log"
 	"net"
 	"users/config"
+	pb "users/internal/proto"
 	"users/internal/service"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type server struct {
+	pb.UserServiceServer
+	service service.Service
+}
 
 func Listen(conf config.Config, service service.Service) {
 	lis, err := net.Listen("tcp", conf.GrpcPort)
@@ -17,11 +26,26 @@ func Listen(conf config.Config, service service.Service) {
 
 	s := grpc.NewServer()
 
-	// logs.RegisterLogServiceServer(s, &LogServer{Models: app.Models})
+	pb.RegisterUserServiceServer(s, &server{
+		service: service,
+	})
 
 	log.Printf("gRPC server listening on %s", conf.GrpcPort)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to listen gRPC: %v", err)
 	}
+}
+
+func (s *server) GetUserById(ctx context.Context, req *pb.GetUserByIdRequest) (*pb.GetUserByIdResponse, error) {
+	user, err := s.service.User().GetByID(int(req.UserId))
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "user not found by id: %d", req.UserId)
+	}
+
+	return &pb.GetUserByIdResponse{
+		UserId:   int32(user.ID),
+		Email:    user.Email,
+		Username: user.Username,
+	}, nil
 }
